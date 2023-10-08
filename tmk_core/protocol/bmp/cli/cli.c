c5a9afb47f8071333538a69e3f21fba216341d9a
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2023-10-08 20:57:44 +0900

    Add dfu command

--- tmk_core/protocol/bmp/cli/cli.c
+++ tmk_core/protocol/bmp/cli/cli.c
@@ -8,8 +8,11 @@
 #include "microshell/util/mscmd.h"
 #include "microshell/util/ntlibc.h"
 
+// QMK headers
 #include "printf.h"
+#include "debug.h"
 
+// BMP headers
 #include "cli.h"
 #include "apidef.h"
 #include "bmp.h"
@@ -17,11 +20,17 @@
 void              cli_puts(const char *str);
 static MICROSHELL microshell;
 static MSCMD      mscmd;
-static cli_app_t cli_app = {NULL, NULL};
+static cli_app_t  cli_app = {NULL, NULL};
 
-static const MSCMD_COMMAND_TABLE table[] = {
+static MSCMD_USER_RESULT usrcmd_help(MSOPT *msopt, MSCMD_USER_OBJECT usrobj);
+static MSCMD_USER_RESULT usrcmd_bootloader(MSOPT *msopt, MSCMD_USER_OBJECT usrobj);
+static MSCMD_USER_RESULT usrcmd_debug_enable(MSOPT *msopt, MSCMD_USER_OBJECT usrobj);
+
+static const MSCMD_COMMAND_TABLE table[] = {{"help", usrcmd_help, "Show this message"},
+                                            {"dfu", usrcmd_bootloader, "Jump to bootloader"},
+                                            {"debug", usrcmd_debug_enable, "Debug print setting"},
 #ifdef USER_DEFINED_MSCMD
-    USER_DEFINED_MSCMD
+                                            USER_DEFINED_MSCMD
 #endif
 };
 
@@ -61,4 +70,46 @@ void cli_exec(void) {
         cur       = buf;
         show_info = false;
     }
+}
+
+static MSCMD_USER_RESULT usrcmd_help(MSOPT *msopt, MSCMD_USER_OBJECT usrobj) {
+    for (int i = 0; i < sizeof(table) / sizeof(table[0]); i++) {
+        cli_puts(table[i].argv0);
+        microshell.uart_putc('\t');
+        cli_puts(table[i].desc);
+        cli_puts("\r\n");
+    }
+    return 0;
+}
+
+static MSCMD_USER_RESULT usrcmd_bootloader(MSOPT *msopt, MSCMD_USER_OBJECT usrobj) {
+    BMPAPI->bootloader_jump();
+    return 0;
+}
+
+static MSCMD_USER_RESULT usrcmd_debug_enable(MSOPT *msopt, MSCMD_USER_OBJECT usrobj) {
+    char arg[16];
+    if (msopt->argc >= 2) {
+        msopt_get_argv(msopt, 1, arg, sizeof(arg));
+        if (strcmp(arg, "on") == 0) {
+            debug_enable = true;
+        } else if (strcmp(arg, "keyboard") == 0) {
+            debug_enable   = true;
+            debug_keyboard = true;
+        } else if (strcmp(arg, "mouse") == 0) {
+            debug_enable = true;
+            debug_mouse  = true;
+        } else if (strcmp(arg, "all") == 0) {
+            debug_enable   = true;
+            debug_keyboard = true;
+            debug_mouse    = true;
+        } else if (strcmp(arg, "off") == 0) {
+            debug_enable   = false;
+            debug_keyboard = false;
+            debug_mouse    = false;
+        }
+    } else {
+        printf("Set debug option [on|off]\r\n");
+    }
+    return 0;
 }

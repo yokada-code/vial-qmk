87a202d6ea8820ca8ce754d8a7abae2bfc355fae
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2023-11-17 21:32:03 +0900

    bmp-vial 1.0.3-rc

--- release.sh
+++ release.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
-version="1_0_2_rc"
+version="1_0_3_rc"
 make ble_micro_pro:vial:zip -j8
 make ble_micro_pro:vial_no_msc:zip -j8
+make sekigon/keyboard_quantizer/kqb:no_msc:zip -j8
 
 mkdir .build/release
 
 unzip -o .build/ble_micro_pro_vial.zip -d .build/release
 unzip -o .build/ble_micro_pro_vial_no_msc.zip -d .build/release
+unzip -o .build/sekigon_keyboard_quantizer_kqb_no_msc.zip -d .build/release
 
 mv .build/release/ble_micro_pro_vial.bin .build/release/ble_micro_pro_vial_${version}.bin
 mv .build/release/ble_micro_pro_vial.dat .build/release/ble_micro_pro_vial_${version}.dat
 mv .build/release/ble_micro_pro_vial_no_msc.bin .build/release/ble_micro_pro_vial_${version}_no_msc.bin
 mv .build/release/ble_micro_pro_vial_no_msc.dat .build/release/ble_micro_pro_vial_${version}_no_msc.dat
+mv .build/release/sekigon_keyboard_quantizer_kqb_no_msc.bin .build/release/sekigon_keyboard_quantizer_kqb_${version}_no_msc.bin
+mv .build/release/sekigon_keyboard_quantizer_kqb_no_msc.dat .build/release/sekigon_keyboard_quantizer_kqb_${version}_no_msc.dat

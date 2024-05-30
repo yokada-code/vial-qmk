cff00668706a287c8b6dcb0b952eaf55875d3cdd
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2024-05-30 21:32:41 +0900

    fix vial definition

--- keyboards/ble_micro_pro/ble_micro_pro.c
+++ keyboards/ble_micro_pro/ble_micro_pro.c
@@ -14,10 +14,23 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     return cont;
 }
 
+void via_custom_value_command_kb(uint8_t *data, uint8_t length) {
+    // data = [ command_id, channel_id, value_id, value_data ]
+    uint8_t *command_id        = &(data[0]);
+    uint8_t *channel_id        = &(data[1]);
+    uint8_t *value_id_and_data = &(data[2]);
+
+    if (*channel_id == id_custom_channel) {
+        printf("command:%d channel:%d value_id:%d value:%d\n", *command_id, *channel_id, value_id_and_data[0], value_id_and_data[1]);
+    }
+}
+
 void raw_hid_receive_kb(uint8_t *data, uint8_t length) {
     uint8_t *command_id = &(data[0]);
     if (*command_id == id_set_keyboard_value) {
         // This version does not handle save flag
+    } else if (*command_id == id_unhandled) {
+        via_custom_value_command_kb(&data[1], length - 1);
     } else {
         *command_id = id_unhandled;
     }

1259c703e0e7a714e41f097bbf3fcd29d4dada17
yokd2018 <43539603+yokada-code@users.noreply.github.com>
2024-08-12 17:02:49 +0900

    KEYMAP: Enable Tri-layer feature
    Implementing tri-layer feature by using layer_state_set_user function.

--- keyboards/ble_micro_pro/keymaps/yokada_crkbd_vial/keymap.c
+++ keyboards/ble_micro_pro/keymaps/yokada_crkbd_vial/keymap.c
@@ -7,3 +7,20 @@ const uint16_t PROGMEM keymaps[1][MATRIX_ROWS][MATRIX_COLS] = {{{
 }}};
 
 const uint16_t PROGMEM encoder_map[1][NUM_ENCODERS][NUM_DIRECTIONS] = {{{KC_MS_U, KC_MS_D}}};
+
+enum crkbd_layers {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    // TRI_LAYER_ENABLE option expects QK_TRI_LAYER_* keycodes.
+    // Such keycoards cannot be used for Tap-Hold keys, so
+    // using this function to implement tri-layers.
+    layer_state_t new;
+    new = update_tri_layer_state(state, _RAISE, _LOWER, _ADJUST);
+    dprintf("state: %u, new: %u\n", (uint8_t)state, (uint8_t)new);
+    return new;
+}
